51184ebe989475d1b4ec3eb175b4b97b5b001cc0
BuvanPranesh <buvanpranesh.kt@gmail.com>
2021-06-23 18:35:23 +0530

    check

--- sample/appengine/flexible/hello/main.py
+++ sample/appengine/flexible/hello/main.py
@@ -4,8 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return 'Hello World'
+    return 'Hello World!'
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
+
+
